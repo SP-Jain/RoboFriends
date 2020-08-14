b24393736c86a2dd86b293584505159501638df8
SP-Jain <jainsparsh2000@gmail.com>
2020-08-14 18:03:44 +0530

    Updates

---
+++ precache-manifest.ccb2eb23103b6468aabc25500d8e1ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17379e6339cd0f0c144908ca09b3f684",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "20b9cc973d28ed59d04c",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4046e47ec47e312784e0",
+    "url": "/RoboFriends/static/css/main.ccdcb3d4.chunk.css"
+  },
+  {
+    "revision": "20b9cc973d28ed59d04c",
+    "url": "/RoboFriends/static/js/2.3a7fa872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.3a7fa872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4046e47ec47e312784e0",
+    "url": "/RoboFriends/static/js/main.d6e82459.chunk.js"
+  },
+  {
+    "revision": "8d1aa98bddb7fe7f6475",
+    "url": "/RoboFriends/static/js/runtime-main.3a4a8caa.js"
+  }
+]);
